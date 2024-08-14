0e1197fdcd0421368550870719c73b651969cb82
Pspritechologist <naaronn@gmail.com>
2024-08-14 08:45:38 -0400

    Prototype gui for sw player

--- src/bin/midis-sw-player.rs
+++ src/bin/midis-sw-player.rs
@@ -1,20 +1,14 @@
 use std::sync::{Arc, Mutex};
 
+use eframe::{App, NativeOptions};
 use rustysynth::{SoundFont, Synthesizer, SynthesizerSettings};
-use midir::{Ignore, MidiIO, MidiInput};
+use midir::{Ignore, MidiIO, MidiInput, MidiInputPort};
 use tinyaudio::{run_output_device, OutputDeviceParameters};
 
 use std::error::Error;
 use std::io::{stdin, stdout, Write};
 
 fn main() {
-    let mut midi_in = MidiInput::new("midis-touch input").expect("Failed to create MIDI input");
-    midi_in.ignore(Ignore::None);
-
-    let in_port = select_port(&midi_in, "input").expect("Failed to select input port");
-
-    let in_port_name = midi_in.port_name(&in_port).unwrap_or_default();
-
     // Setup the audio output.
     let params = OutputDeviceParameters {
         channels_count: 2,
@@ -30,20 +24,33 @@ fn main() {
 
     let synth_c = synthesizer.clone();
 
-    // _conn_in needs to be a named parameter, because it needs to be kept alive until the end of the scope
-    let _conn_in = midi_in.connect(
-        &in_port,
-        "midis-touch input",
-        move |_, message, _| {
-            synth_c.lock().unwrap().process_midi_message(
-                1, 
-                message[0] as i32, 
-                message.get(1).cloned().unwrap_or_default() as i32, 
-                message.get(2).cloned().unwrap_or_default() as i32
-            );
-        },
-        (),
-    ).expect("Failed to connect to MIDI input port");
+    let mut midi_in = MidiInput::new("midis-touch input").expect("Failed to create MIDI input");
+    midi_in.ignore(Ignore::None);
+
+    #[cfg(not(feature = "gui"))] let _conn_in;
+    #[cfg(not(feature = "gui"))] let in_port_name;
+
+    #[cfg(not(feature = "gui"))]
+    {
+        let in_port = select_port(&midi_in, "input").expect("Failed to select input port");
+    
+        in_port_name = midi_in.port_name(&in_port).unwrap_or_default();
+
+        // _conn_in needs to be a named parameter, because it needs to be kept alive until the end of the scope
+        _conn_in = midi_in.connect(
+            &in_port,
+            "midis-touch input",
+            move |_, message, _| {
+                synth_c.lock().unwrap().process_midi_message(
+                    1, 
+                    message[0] as i32, 
+                    message.get(1).cloned().unwrap_or_default() as i32, 
+                    message.get(2).cloned().unwrap_or_default() as i32
+                );
+            },
+            (),
+        ).expect("Failed to connect to MIDI input port");
+    }
 
     // Buffer for the audio output.
     let mut left: Vec<f32> = vec![0_f32; params.channel_sample_count];
@@ -53,22 +60,31 @@ fn main() {
     let _dev = run_output_device(params, move |data| {
         synthesizer.lock().unwrap().render(&mut left[..], &mut right[..]);
         for (i, ch) in data.chunks_mut(2).enumerate() {
-            // println!("Outputting...");
             ch[0] = left[i];
             ch[1] = right[i];
         }
     }).expect("Failed to output audio.");
 
+    #[cfg(feature = "gui")]
+    {
+        let mut app = gui::AppWindow::new(midi_in, synth_c);
 
-    println!(
-        "Taking input from '{}' (press enter to exit) ...",
-        in_port_name
-    );
+        eframe::run_simple_native("Midis SW Player", NativeOptions::default(), move |ctx, frame| app.update(ctx, frame)).expect("GUI Error");
+    }
 
-    let mut input = String::new();
-    _ = stdin().read_line(&mut input); // wait for next enter key press
+    #[cfg(not(feature = "gui"))]
+    {
+        println!(
+            "Taking input from '{}' (press enter to exit) ...",
+            in_port_name
+        );
+
+        let mut input = String::new();
+        _ = stdin().read_line(&mut input); // wait for next enter key press
+    }
 }
 
+#[cfg(not(feature = "gui"))]
 fn select_port<T: MidiIO>(midi_io: &T, descr: &str) -> Result<T::Port, Box<dyn Error>> {
     println!("Available {} ports:", descr);
     let midi_ports = midi_io.ports();
@@ -83,4 +99,81 @@ fn select_port<T: MidiIO>(midi_io: &T, descr: &str) -> Result<T::Port, Box<dyn E
         .get(input.trim().parse::<usize>()?)
         .ok_or("Invalid port number")?;
     Ok(port.clone())
+}
+
+#[cfg(feature = "gui")]
+mod gui {
+    use super::*;
+    use eframe::egui::{self, CentralPanel, ComboBox};
+    use midir::MidiInputConnection;
+
+    pub struct AppWindow {
+        pub midi_input: MidiInput,
+        pub synth: Arc<Mutex<Synthesizer>>,
+        pub connection: Option<MidiInputConnection<()>>,
+
+        combo: bool,
+        selected: usize,
+    }
+
+    impl AppWindow {
+        pub fn new(midi_input: MidiInput, synth: Arc<Mutex<Synthesizer>>) -> Self {
+            Self {
+                midi_input,
+                synth,
+                connection: None,
+                combo: false,
+                selected: 0,
+            }
+        }
+
+        fn make_connection(&mut self, port: &MidiInputPort) {
+            let synth_c = self.synth.clone();
+            self.connection = Some(MidiInput::new("temp").unwrap().connect(
+                &port,
+                "midis-touch input",
+                move |_, message, _| {
+                    synth_c.lock().unwrap().process_midi_message(
+                        1, 
+                        message[0] as i32, 
+                        message.get(1).cloned().unwrap_or_default() as i32, 
+                        message.get(2).cloned().unwrap_or_default() as i32
+                    );
+                },
+                (),
+            ).expect("Failed to connect to MIDI input port"));
+        }
+    }
+
+    impl eframe::App for AppWindow {
+        fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+            CentralPanel::default().show(ctx, |ui| {
+                ui.horizontal(|ui| {
+                    ui.heading("Midi Player :)");
+                    ui.checkbox(&mut self.combo, "Dropdown");
+                });
+
+                if self.combo {
+                    if ComboBox::from_label("Select input").show_index(ui, &mut self.selected, self.midi_input.port_count(), |i| {
+                        self.midi_input.port_name(&self.midi_input.ports()[i]).unwrap_or_default()
+                    }).changed() {
+                        self.make_connection(&self.midi_input.ports()[self.selected]);
+                    }
+                } else {
+                    ui.menu_button("Change input", |ui| {
+                        if ui.button("Close").clicked() {
+                            ui.close_menu();
+                        }
+                        ui.vertical_centered(move|ui| {
+                            for port in self.midi_input.ports().iter() {
+                                if ui.button(self.midi_input.port_name(port).unwrap_or("ERROR".into())).clicked() {
+                                    self.make_connection(port);
+                                }
+                            }
+                        });
+                    });
+                }
+            });
+        }
+    }
 }
